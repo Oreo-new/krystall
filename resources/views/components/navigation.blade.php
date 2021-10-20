ee529fcffcbabaa425163ebe049a4432efec64d2
jayrus <jay.pardillo@copypaste.ph>
2021-10-20 23:53:44 +0800

    working

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,5 +1,5 @@
 <div class="container mx-auto">
-  <div id="btn-nav" class="group cursor-pointer w-10" onclick="openNav(); open()">
+  <div id="btn-nav" class="group cursor-pointer w-10" onclick="openNav()">
     <span class="group-hover:text-green-400 duration-100">Meny</span>
     <span class="bg-black block m-1 h-1 w-8 group-hover:bg-green-400 duration-100"></span>
     <span class="bg-black block m-1 h-1 w-8 group-hover:bg-green-400 duration-100"></span>
@@ -7,7 +7,7 @@
     <span class="bg-black block m-1 h-1 w-8 group-hover:bg-green-400 duration-100"></span>
   </div>
   <div class="">
-    <ul id="mySidenav" class="bg-blue-600 fixed block top-0 right-0 h-full w-0">
+    <ul id="mySidenav" class="bg-blue-600 fixed top-0 right-0 h-full w-0">
       <a href="javascript:void(0)" class="" onclick="closeNav()">x</a>
       <li><a href="">Hjem</a></li>
       <li><a href="">Veildere alfabetisk</a></li>
@@ -23,8 +23,9 @@
 @once
     @push('scripts')
         <script>
-          function openNav(){
-            document.getElementById("mySidenav").style.width = "200px";
+
+          function openNav(){ 
+            document.getElementById("mySidenav").style.display = "block";
             document.getElementById("body").style.marginRight = "200px";
 
           }
@@ -33,8 +34,17 @@
             document.getElementById("mySidenav").style.width = "0px";
             document.getElementById("body").style.marginRight = "0px";
           }
-
-
+          
+          // var nav;
+          // function openNav(){
+          //   if ( nav == 1) {
+          //     document.getElementById("mySidenav").style.display = "block";
+          //     return nav = 0;
+          //   } else {
+          //     document.getElementById("mySidenav").style.display = "none";
+          //     return nav = 1;
+          //   }
+          // }
 
         </script>
     @endpush
