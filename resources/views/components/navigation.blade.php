f2386d8ecc915aeb9a4ef7d09007f9472891661e
jayrus <jay.pardillo@copypaste.ph>
2021-10-22 13:06:19 +0800

    add

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,22 +1,27 @@
-<div class="container mx-auto">
-  <div id="btn-nav" class="group cursor-pointer w-10" onclick="openNav()">
-    <span class="group-hover:text-green-400 duration-100">Meny</span>
-    <span class="bg-black block m-1 h-1 w-8 group-hover:bg-green-400 duration-100"></span>
-    <span class="bg-black block m-1 h-1 w-8 group-hover:bg-green-400 duration-100"></span>
-    <span class="bg-black block m-1 h-1 w-8 group-hover:bg-green-400 duration-100"></span>
-    <span class="bg-black block m-1 h-1 w-8 group-hover:bg-green-400 duration-100"></span>
-  </div>
-  <div class="relative">
-    <ul id="mySidenav" class="bg-green-700 p-12 pt-24 top-0 h-full block w-60 fixed -right-60" style="transition: right ease-in-out .3s;">
-      <a href="javascript:void(0)" class="text-white text-2xl font-bold leading-8 absolute p-5 top-0 right-0" onclick="closeNav()">x</a>
-      <li><a href="" class="text-white text-xl leading-8 ">Hjem</a></li>
-      <li><a href="" class="text-white text-xl leading-8 ">Veildere alfabetisk</a></li>
-      <li><a href="" class="text-white text-xl leading-8 ">Medium magasin</a></li>
-      <li><a href="" class="text-white text-xl leading-8 ">Dagshoroskop</a></li>
-      <li><a href="" class="text-white text-xl leading-8 ">Artikkelarkiv</a></li>
-      <li><a href="" class="text-white text-xl leading-8 ">Ordbok</a></li>
-      <li><a href="" class="text-white text-xl leading-8 ">Prisopplysning</a></li>
-    </ul>
+<div class="p-5 bg-center bg-cover bg-auto bg-no-repeat" style="background-image: url('{{ asset('images/Bakgrunnsbilde.jpg') }}'); background-size: 100% 100%">
+  <div class="container mx-auto flex align-center justify-center relative">
+    <div>
+      <img src="{{ asset('images/logo_white.png') }}" alt="logo">
+    </div>
+    <div id="btn-nav" class="absolute right-0 group cursor-pointer w-20" onclick="openNav()">
+      <span class="text-white group-hover:text-green-400 duration-100">Meny</span>
+      <span class="bg-white block m-1 h-1 w-8 group-hover:bg-green-400 duration-100"></span>
+      <span class="bg-white block m-1 h-1 w-8 group-hover:bg-green-400 duration-100"></span>
+      <span class="bg-white block m-1 h-1 w-8 group-hover:bg-green-400 duration-100"></span>
+      <span class="bg-white block m-1 h-1 w-8 group-hover:bg-green-400 duration-100"></span>
+    </div>
+    <div class="relative">
+      <ul id="mySidenav" class="bg-green-700 p-10 py-24 top-0 h-full block w-72 fixed -right-72" style="transition: right ease .3s; box-shadow: -2px 0 4px -1px rgba(0, 0, 0, 0.5);">
+        <a href="javascript:void(0)" class="text-white text-2xl font-bold leading-8 absolute p-5 top-0 right-0" onclick="closeNav()">x</a>
+        <li><a href="" class="text-blue-400 font-medium text-xl leading-10 hover:text-blue-400 transition duration-150 ">Hjem</a></li>
+        <li><a href="" class="text-white font-medium text-xl leading-10 hover:text-blue-400 transition duration-150 ">Veildere alfabetisk</a></li>
+        <li><a href="" class="text-white font-medium text-xl leading-10 hover:text-blue-400 transition duration-150 ">Medium magasin</a></li>
+        <li><a href="" class="text-white font-medium text-xl leading-10 hover:text-blue-400 transition duration-150 ">Dagshoroskop</a></li>
+        <li><a href="" class="text-white font-medium text-xl leading-10 hover:text-blue-400 transition duration-150 ">Artikkelarkiv</a></li>
+        <li><a href="" class="text-white font-medium text-xl leading-10 hover:text-blue-400 transition duration-150 ">Ordbok</a></li>
+        <li><a href="" class="text-white font-medium text-xl leading-10 hover:text-blue-400 transition duration-150 ">Prisopplysning</a></li>
+      </ul>
+    </div>
   </div>
 </div>
 
@@ -30,13 +35,13 @@
             
             let mySidenav = document.getElementById('mySidenav');
 
-            if (mySidenav.classList.contains('-right-60')) {
+            if (mySidenav.classList.contains('-right-72')) {
 
-              mySidenav.classList.remove("-right-60");
+              mySidenav.classList.remove("-right-72");
               mySidenav.classList.add("right-0");
-              body.style.marginRight = "250px";
+              body.style.marginRight = "280px";
                 body.style.transition = "right";
-                  body.style.transition = "ease-in-out";
+                  body.style.transition = "ease";
                     body.style.transition = ".3s";
 
             } else {
@@ -48,7 +53,7 @@
 
           function closeNav(){
             mySidenav.classList.remove("right-0");
-            mySidenav.classList.add("-right-60");
+            mySidenav.classList.add("-right-72");
             body.style.marginRight = "0px";
               body.style.transition = "left";
                 body.style.transition = "ease-in-out";
--- resources/views/components/top-boxes.blade.php
+++ resources/views/components/top-boxes.blade.php
@@ -1,13 +1,20 @@
 <div class="container mx-auto px-4">
     <div class="flex w-100 ">
         <div class="w-3/12 mx-2 my-4"><a href=""> <img src="{{ asset('images/krys1.jpg') }}" alt="tag"> </a></div>
+        <div class="w-3/12 mx-2 my-4"><a href=""> <img src="{{ asset('images/95158-frida.jpg') }}" alt="tag"> </a></div>
+        <div class="w-3/12 mx-2 my-4"><a href=""> <img src="{{ asset('images/95236-astarah-torill-anita-prepaid.jpg') }}" alt="tag"> </a></div>
         <div class="w-3/12 mx-2 my-4"><a href=""> <img src="{{ asset('images/krys1.jpg') }}" alt="tag"> </a></div>
         <div class="w-3/12 mx-2 my-4"><a href=""> <img src="{{ asset('images/krys1.jpg') }}" alt="tag"> </a></div>
         <div class="w-3/12 mx-2 my-4"><a href=""> <img src="{{ asset('images/krys1.jpg') }}" alt="tag"> </a></div>
-        <div class="w-3/12 mx-2 my-4"><a href=""> <img src="{{ asset('images/krys1.jpg') }}" alt="tag"> </a></div>
-        <div class="w-3/12 mx-2 my-4"><a href=""> <img src="{{ asset('images/krys1.jpg') }}" alt="tag"> </a></div>
-        <div class="w-3/12 mx-2 my-4"><a href=""> <img src="{{ asset('images/krys1.jpg') }}" alt="tag"> </a></div>
-        <div class="w-3/12 mx-2 my-4"><a href=""> <img src="{{ asset('images/krys1.jpg') }}" alt="tag"> </a></div>
+        <div class="w-3/12 mx-2 my-4"><a href=""> <img src="{{ asset('images/99019-margaret-ny.jpg') }}" alt="tag"> </a></div>
+        <div class="w-3/12 mx-2 my-4"><a href=""> <img src="{{ asset('images/95556-cathinka.jpg') }}" alt="tag"> </a></div>
         <div class="w-3/12 mx-2 my-4"><a href=""> <img src="{{ asset('images/krys1.jpg') }}" alt="tag"> </a></div>
     </div>
 </div>
+@once
+    @push('scripts')
+        <script>
+            
+        </script>
+    @endpush
+@endonce
