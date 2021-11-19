a959c945ddba46767b64b0b6162cfc54e5403ec2
jayrus <jay.pardillo@copypaste.ph>
2021-11-19 13:49:53 +0800

    add footer and medium screen responsive

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -58,11 +58,52 @@
 				
 			</main>
 
-			<footer>
+			<footer class="bg-green-500">
 				{{-- <x-footer></x-footer> --}}
+				<div class="container mx-auto px-6">
+					<div class="flex flex-wrap flex-col justify-between py-6 sm:flex-row md:mx-2">
+						<div class="w-full m-2 p-2 text-white md:w-56 md:m-0 md:p-0 lg:w-1/3">
+							<p class="w-4/5 mx-auto md:m-0 md:w-full lg:w-4/5">
+								<a href="">
+									<img src="{{ asset('images/logo_white.png') }}" alt="">
+								</a>
+							</p>
+							<p class="w-3/5 mx-auto text-center mt-5 text-sm font-medium md:w-full md:text-left md:mt-28 lg:mx-0 xl:w-2/3">
+								Copyright © 2017 SanaVia AS. All rights reserved. Info: 22004600
+							</p>
+						</div>
+						<div class="w-fulll h-full m-2 p-2 text-white font-medium flex flex-wrap flex-col items-center gap-x-3 sm:items-start sm:w-72 sm:h-36 md:gap-x-4 md:w-48 md:m-0 md:p-0 md:leading-8 lg:w-1/3">
+							<a href="" class="underline hover:no-underline"> <span>Hjem</span> </a>					
+							<a href="" class="underline hover:no-underline md:leading-normal lg:leading-8"> <span>Veiledere<br class="hidden sm:block lg:hidden"> alfabetisk</span> </a>
+							<a href="" class="underline hover:no-underline"> <span>Medium magasin</span> </a>
+							<a href="" class="underline hover:no-underline"> <span>Dagshoroskop</span> </a>
+							<a href="" class="underline hover:no-underline"> <span>Artikkelarkiv</span> </a>
+							<a href="" class="underline hover:no-underline"> <span>Ordbok</span> </a>
+							<a href="" class="underline hover:no-underline"> <span>Prisopplysning</span> </a>
+							<a href="" class="underline hover:no-underline"> <span>Personvernerklæring</span> </a>
+						</div>
+						<div class="w-full text-center m-2 p-2 text-white sm:text-left sm:w-1/4 sm:h-1/2 md:w-1/4 md:text-right md:w-32 md:m-0 md:p-0 md:leading-9">
+							<li class="list-none underline hover:no-underline" onclick="topFunction()"><a id="top" href="">Til toppen</a></li>
+							<li class="list-none underline hover:no-underline"><a href="">Besok oss pa Facebook</a></li>
+						</div>
+					</div>
+				</div>
+
 			</footer>
 
 		</div>
+		@once
+		@push('scripts')
+		<script>
+			var Till_toppen = document.getElementById("top");
+
+			function topFunction() {
+			document.body.scrollTop = 0;
+			document.documentElement.scrollTop = 0;
+			}
+		</script>
+		@endpush
+		@endonce
 
 		{{-- @livewireScripts --}}
 
@@ -70,6 +111,7 @@
 		<script src="{{ asset('js/app.js') }}"></script>
 		<script src="{{ asset('js/flipster.min.js') }}"></script>
 		<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.3.5/dist/alpine.min.js" defer></script>
+		{{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> --}}
 		{{-- <script defer>
 			AOS.init({
 				disable: 'mobile',
--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -1,27 +1,50 @@
 @section('title', 'Hjem' )
 
 <x-app-layout>
-<div class="w-full md:p-2 md:m-2 md:flex md:flex-wrap lg:flex-nowrap lg:flex-col lg:m-0 lg:p-0 md:space-x-4 lg:space-x-0 md:justify-between">
+<div class="top-items w-full lg:p-2 lg:m-2 md:flex md:flex-wrap lg:flex-nowrap lg:flex-col lg:m-0 lg:p-0 md:justify-between">
 
     @foreach (range(1, 2) as $item)
-        <div class="flex flex-col-reverse w-full py-5 border-t-2 justify-between md:w-2/5 lg:w-full  lg:px-0 lg:flex-row lg:justify-between">
-            <div class="w-full lg:w-1/2">
-                <div>
-                    <p class="text-2xl md:text-xl font-bold group-hover:text-green-500 duration-300">
-                        <a href="">Veilederen Tina bruker kun sine synske evner.</a>
-                    </p>
+    
+        @if($loop->iteration % 2 == 0)
+            <div class="flex flex-col-reverse w-full py-5 justify-between border-t-2 md:w-1/2 lg:w-full lg:px-0 lg:flex-row lg:justify-between even">
+                <div class="w-full lg:w-1/2">
+                    <div>
+                        <p class="text-2xl md:text-xl font-bold group-hover:text-green-500 duration-300">
+                            <a href="">Veilederen Tina bruker kun sine synske evner.</a>
+                        </p>
+                    </div>
+                    <div>
+                        <p class="text-sm">Tina er av sigøyner-/taterslekt. Hun har skjønt at hennes nedarvede blodslinjer forsterker hennes synske evner. Den dagen hun begynte å…</p> 
+                    </div>
+                    <div class="">
+                        <a href="" class="text-green-500 hover:text-black duration-300">Les mer...</a>
+                    </div>
                 </div>
-                <div>
-                    <p class="text-sm">Tina er av sigøyner-/taterslekt. Hun har skjønt at hennes nedarvede blodslinjer forsterker hennes synske evner. Den dagen hun begynte å…</p> 
+                <div class="w-full lg:w-1/2">
+                    <a href="" class="block h-80 md:h-44 bg-contain sm:bg-cover lg:h-48 xl:h-64 bg-center bg-no-repeat" style="background-image: url('{{ asset('images/6ddb2450462828abf9aabc88d6bfb7fe_L.jpg') }}')"></a><!-- <img class="h-1/2" src="{{ asset('images/6ddb2450462828abf9aabc88d6bfb7fe_L.jpg') }}" alt=""> -->
                 </div>
-                <div class="">
-                    <a href="" class="text-green-500 hover:text-black duration-300">Les mer...</a>
+            </div> 
+        @else
+            <div class="flex flex-col-reverse w-full py-5 justify-between border-t-2 md:w-1/2 lg:w-full lg:px-0 lg:flex-row lg:justify-between odd">
+                <div class="w-full lg:w-1/2">
+                    <div>
+                        <p class="text-2xl md:text-xl font-bold group-hover:text-green-500 duration-300">
+                            <a href="">Veilederen Tina bruker kun sine synske evner.</a>
+                        </p>
+                    </div>
+                    <div>
+                        <p class="text-sm">Tina er av sigøyner-/taterslekt. Hun har skjønt at hennes nedarvede blodslinjer forsterker hennes synske evner. Den dagen hun begynte å…</p> 
+                    </div>
+                    <div class="">
+                        <a href="" class="text-green-500 hover:text-black duration-300">Les mer...</a>
+                    </div>
                 </div>
-            </div>
-            <div class="w-full lg:w-1/2">
-                <a href="" class="block h-80 md:h-44 bg-contain sm:bg-cover lg:h-48 xl:h-64 bg-center bg-no-repeat" style="background-image: url('{{ asset('images/6ddb2450462828abf9aabc88d6bfb7fe_L.jpg') }}')"></a><!-- <img class="h-1/2" src="{{ asset('images/6ddb2450462828abf9aabc88d6bfb7fe_L.jpg') }}" alt=""> -->
-            </div>
-        </div>  
+                <div class="w-full lg:w-1/2">
+                    <a href="" class="block h-80 md:h-44 bg-contain sm:bg-cover lg:h-48 xl:h-64 bg-center bg-no-repeat" style="background-image: url('{{ asset('images/6ddb2450462828abf9aabc88d6bfb7fe_L.jpg') }}')"></a><!-- <img class="h-1/2" src="{{ asset('images/6ddb2450462828abf9aabc88d6bfb7fe_L.jpg') }}" alt=""> -->
+                </div>
+            </div> 
+        @endif
+
     @endforeach
 
         <div class="w-full h-full py-5 border-t-2">
@@ -29,41 +52,50 @@
                 <img src="{{ asset('images/68497d6cb194485d2759fde9466457b7_XL.jpg') }}" alt="">
             </a>
         </div>
-        <div class="flex flex-col-reverse w-full py-5 md:px-2 border-t-2 justify-between md:justify-end items-start group md:w-1/2 lg:px-0 lg:flex-row lg:w-full lg:justify-between">
-            <div class="w-full  lg:w-1/2 xl:w-96">
-                <div>
-                    <p class="text-2xl md:text-xl font-bold group-hover:text-green-500 duration-300">
-                        <a href="">Synske Amanda June gir deg svar!</a>
-                    </p>
+    
+    @foreach (range(1, 6) as $item)
+
+        @if($loop->iteration % 2 == 0)
+            <div class="flex flex-col-reverse w-full py-5 justify-between border-t-2 md:w-1/2 lg:w-full lg:px-0 lg:flex-row lg:justify-between even">
+                <div class="w-full lg:w-1/2">
+                    <div>
+                        <p class="text-2xl md:text-xl font-bold group-hover:text-green-500 duration-300">
+                            <a href="">Veilederen Tina bruker kun sine synske evner.</a>
+                        </p>
+                    </div>
+                    <div>
+                        <p class="text-sm">Tina er av sigøyner-/taterslekt. Hun har skjønt at hennes nedarvede blodslinjer forsterker hennes synske evner. Den dagen hun begynte å…</p> 
+                    </div>
+                    <div class="">
+                        <a href="" class="text-green-500 hover:text-black duration-300">Les mer...</a>
+                    </div>
                 </div>
-                <div>
-                    <p class="text-sm">Godt voksen dame med mange års erfaring på teletorget.Amanda June er i en egen klasse, når det gjelder bra tilbakemeldinger… </p> 
+                <div class="w-full lg:w-1/2">
+                    <a href="" class="block h-80 md:h-44 bg-contain sm:bg-cover lg:h-48 xl:h-64 bg-center bg-no-repeat" style="background-image: url('{{ asset('images/6ddb2450462828abf9aabc88d6bfb7fe_L.jpg') }}')"></a><!-- <img class="h-1/2" src="{{ asset('images/6ddb2450462828abf9aabc88d6bfb7fe_L.jpg') }}" alt=""> -->
                 </div>
-                <div class="">
-                    <a href="" class="text-green-500 hover:text-black duration-300">Les mer...</a>
+            </div> 
+        @else
+            <div class="flex flex-col-reverse w-full py-5 justify-between border-t-2 md:w-1/2 lg:w-full lg:px-0 lg:flex-row lg:justify-between odd">
+                <div class="w-full lg:w-1/2">
+                    <div>
+                        <p class="text-2xl md:text-xl font-bold group-hover:text-green-500 duration-300">
+                            <a href="">Veilederen Tina bruker kun sine synske evner.</a>
+                        </p>
+                    </div>
+                    <div>
+                        <p class="text-sm">Tina er av sigøyner-/taterslekt. Hun har skjønt at hennes nedarvede blodslinjer forsterker hennes synske evner. Den dagen hun begynte å…</p> 
+                    </div>
+                    <div class="">
+                        <a href="" class="text-green-500 hover:text-black duration-300">Les mer...</a>
+                    </div>
                 </div>
-            </div>
-            <div class="w-full  lg:w-1/2">
-                <a href="" class="block h-80 bg-contain md:h-44 sm:bg-cover md:bg-contain lg:bg-contain lg:h-48 xl:bg-cover xl:h-64 bg-center bg-no-repeat" style="background-image: url('{{ asset('images/6cdb4ac6ccf86fc9922b1b1ecf5faa0d_M.jpg') }}')"></a><!-- <img class="" src="{{ asset('images/6cdb4ac6ccf86fc9922b1b1ecf5faa0d_M.jpg') }}" alt=""> -->
-            </div>
-        </div>
-        <div class="flex flex-col-reverse w-full py-5 md:px-2 border-t-2 justify-between md:justify-end items-start group md:w-1/2 lg:px-0 lg:flex-row lg:w-full lg:justify-between">
-            <div class="w-full  lg:w-1/2 xl:w-96">
-                <div>
-                    <p class="text-2xl md:text-xl font-bold group-hover:text-green-500 duration-300">
-                        <a href="">Siri er diskré, ærlig og hjelper deg uten fordommer.</a>
-                    </p>
+                <div class="w-full lg:w-1/2">
+                    <a href="" class="block h-80 md:h-44 bg-contain sm:bg-cover lg:h-48 xl:h-64 bg-center bg-no-repeat" style="background-image: url('{{ asset('images/6ddb2450462828abf9aabc88d6bfb7fe_L.jpg') }}')"></a><!-- <img class="h-1/2" src="{{ asset('images/6ddb2450462828abf9aabc88d6bfb7fe_L.jpg') }}" alt=""> -->
                 </div>
-                <div>
-                    <p class="text-sm">Siri begynte på teletorget i januar 2020 etter å ha jobbet mange år som medium i utlandet, og ble raskt…</p> 
-                </div>
-                <div class="">
-                    <a href="" class="text-green-500 hover:text-black duration-300">Les mer...</a>
-                </div>
-            </div>
-            <div class="w-full  lg:w-1/2">
-                <a href="" class="block h-80 bg-contain md:h-44 sm:bg-cover md:bg-contain lg:bg-contain lg:h-48 xl:bg-cover xl:h-64 bg-center bg-no-repeat" style="background-image: url('{{ asset('images/78a1f76203c562c3d7c318765865da44_L.jpg') }}')"></a><!-- <img class="" src="{{ asset('images/78a1f76203c562c3d7c318765865da44_L.jpg') }}" alt=""> -->
-            </div>
-        </div>
+            </div> 
+        @endif
+
+    @endforeach
+
     </div>
 </x-app-layout>
