10c4911d27c5808b5faf9574de48c70e72993173
jayrus <jay.pardillo@copypaste.ph>
2021-11-23 16:04:31 +0800

    add artikkelarkiv page

--- resources/views/pages/artikkel.blade.php
+++ resources/views/pages/artikkel.blade.php
@@ -3,6 +3,38 @@
 
 <x-app-layout>
 
-
+<div class="mt-4">
+    <div class="my-4">
+        <p class="text-center text-2xl font-semibold sm:text-3xl md:font-normal">Veilderene Tina bruker kun sine synske evner.</p>
+    </div>
+    <div class="py-2 w-full h-auto">
+        <img class="w-full" src="{{asset('/images/6ddb2450462828abf9aabc88d6bfb7fe_L.jpg')}}" alt="">
+    </div>
+    <div class="text-center flex flex-col sm:hidden">
+        <a class="text-green-500 text-xl font-bold" href="">92995371</a>
+        <a class="text-green-500 text-xl font-bold" href="">21058771</a>
+    </div>
+    <div class="py-2">
+        <p class="font-bold text-sm font-semibold">Tina er av sigøyner-/taterslekt. Hun har skjønt at hennes nedarvede blodslinjer forsterker hennes synske evner. 
+            Den dagen hun begynte å jobbe på teletorget følte Tina at hun endelig var kommet «hjem.» Hun bruker ingen 
+            hjelpemidler når hun arbeider, kun sine egne evner. Mange års studier innenfor psykologi har hjulpet mer enn 
+            kort, pendler og andre hjelpemidler. Tina gir coaching og rådgivning innenfor områder som bl.a. rus og psykiatri, 
+            samt for barn med psykiatriske lidelser.
+        </p>
+    </div>
+    <div class="my-6">
+        <p class="text-lg mb-6">Ønsker du å snakke med Tina, er hun tilgjengelig hver dag, også helg, 
+            fra kl ni om morgenen og til kl 24 om kvelden.
+        </p>
+        <span class="font-bold text-lg">
+            <a class="text-lg font-bold text-blue-700 md:text-base" href="">829 95 152</a>(kr.29,90 pr min) 
+        </span>
+        <p class="text-xl md:text-base mb-6">Betaling via mobileregning og faktura</p>
+        <span class="font-bold text-lg">
+            <a class="text-lg font-bold text-blue-700 md:text-base" href="">21 05 87 51</a>(kr.29,90 pr min)
+        </span>
+        <p class="text-xl md:text-base">Kortbetaling via <span><a class="text-green-500" href="">veildertjenesten.no</a></span></p>
+    </div>
+</div>
   
 </x-app-layout>
--- resources/views/pages/artikkelarkiv.blade.php
+++ resources/views/pages/artikkelarkiv.blade.php
@@ -3,6 +3,15 @@
 
 <x-app-layout>
 
-
+<div class="mt-4">
+    <div class="w-full mb-8">
+        <p class="text-2xl text-center">Alle artikler:</p>
+    </div>
+    @foreach (range(1,4) as $item)
+        <div class="my-3">
+            <a class="text-green-500 font-medium text-lg md:text-2xl" href="">Veilederen Tina bruker kun sine synske evner.</a>
+        </div>
+    @endforeach
+</div>
   
 </x-app-layout>
